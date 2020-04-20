45480169cc8184690b980a30c535b399259c97ec
Stephanie Ballard <user.name>
2020-04-19 21:09:50 -0700

    segue finished

---
+++ BMI Calculator/Controllers/CalculateViewController.swift
@@ -0,0 +1,60 @@
+//
+//  CalculateViewController.swift
+//  BMI Calculator
+//
+//  Created by Angela Yu on 21/08/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+    
+    var bmiValue = "0.0"
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        heightLabel.text = String(format: "%.2f", sender.value) + "m"
+        //print(String(format: "%.2f", sender.value))
+    }
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightLabel.text = String(format: "%.2f", sender.value) + "Kg"
+        //print(Int(sender.value))
+    }
+    
+    @IBAction func calculateButtonTapped(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        let bmi = weight / (height * height)
+        bmiValue = String(format: "%.1f", bmi)
+//        let bmi = weight / pow(height, 2)
+//        print(bmi)
+        
+//        let secondVC = SecondViewController()
+//        secondVC.bmiValue = String(format: "%.1f", bmi)
+//
+//        self.present(secondVC, animated: true, completion: nil)
+        
+        self.performSegue(withIdentifier: "ResultsModalSegue", sender: self)
+    }
+    
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ResultsModalSegue" {
+            guard let destinationVC = segue.destination as? ResultsViewController else { return }
+            destinationVC.bmiValue = bmiValue
+        }
+    }
+}
+
+ 
